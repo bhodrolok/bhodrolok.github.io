a88fedd2c9919064f21a4e2f801e4a7f0a851e3d
bhodrolok <ranadipchatterji+githubUC1@gmail.com>
2023-12-17 12:41:58 +0530

    feat: Update fonts
    - separate for rest of body & footer

--- static/js/main.js
+++ static/js/main.js
@@ -361,6 +361,26 @@ function getPageSourceGH(){
   }
 }
 
+function enableAudio(){
+  // Good ref I think: https://dobrian.github.io/cmp/topics/sample-recording-and-playback-with-web-audio-api/1.loading-and-playing-sound-files.html
+  const audioElement = document.getElementById('audio-1');
+  var audioSrc = `audio/music.mp3`;
+  
+  const audio = new Audio(audioSrc);
+  const audioIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22"><path fill="currentColor" d="M15 4.58152V12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C12.3506 9 12.6872 9.06016 13 9.17071V2.04938C18.0533 2.5511 22 6.81465 22 12C22 17.5229 17.5228 22 12 22C6.47715 22 2 17.5229 2 12C2 6.81465 5.94668 2.5511 11 2.04938V4.0619C7.05369 4.55399 4 7.92038 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 8.64262 17.9318 5.76829 15 4.58152Z"></path></svg>`
+  const fmtAudio = ` ${audioIconSVG}`;
+  const eventType = [ "canplay", "canplaythrough" ];
+  
+  if (audioElement){
+    audioElement.innerHTML = fmtAudio; 
+    //audio.loop = true;
+    // audio.play();
+    audio.addEventListener(eventType[0], () => {
+      audio.play();
+    });
+  }
+}
+
 //--------------------------------------------
 
 enableThemeToggle();
@@ -381,3 +401,4 @@ showBuildSpecs();
 generateDayGreeting();
 updateCommitInfo();
 getPageSourceGH();
+//enableAudio();
